d388284e8da7b8f9ebefa6df99bcaab28f1c1a57
Ariq Khoiri <qq.khoiri@gmail.com>
2023-03-23 20:41:27 +0700

    refactor feign

--- currency-conversion-service/src/main/java/com/qirara/currencyconversionservice/proxy/CurrencyExchangeProxy.java
+++ currency-conversion-service/src/main/java/com/qirara/currencyconversionservice/proxy/CurrencyExchangeProxy.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
-@FeignClient(name = "currency-exchange", url = "http://localhost:8000")
+@FeignClient(name = "currency-exchange")
 public interface CurrencyExchangeProxy {
 
     @RequestMapping(method = RequestMethod.GET, path = "/currency-exchange/from/{from}/to/{to}", consumes = "application/json")
