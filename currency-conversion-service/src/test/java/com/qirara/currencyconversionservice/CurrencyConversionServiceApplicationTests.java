6c5c85cbc73247ab0af2be2f6cf826f8755f2f72
Ariq Khoiri <qq.khoiri@gmail.com>
2023-03-23 08:37:37 +0700

    feat:currency conversion service

---
+++ currency-conversion-service/src/test/java/com/qirara/currencyconversionservice/CurrencyConversionServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.qirara.currencyconversionservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CurrencyConversionServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
