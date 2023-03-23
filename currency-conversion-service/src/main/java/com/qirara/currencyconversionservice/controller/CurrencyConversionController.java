3303b69ebbf21ae0fea81e4aef324a5b030c1744
Ariq Khoiri <qq.khoiri@gmail.com>
2023-03-23 11:29:40 +0700

    feat:fetch currency-exchange service using feign

--- currency-conversion-service/src/main/java/com/qirara/currencyconversionservice/controller/CurrencyConversionController.java
+++ currency-conversion-service/src/main/java/com/qirara/currencyconversionservice/controller/CurrencyConversionController.java
@@ -1,6 +1,7 @@
 package com.qirara.currencyconversionservice.controller;
 
 import com.qirara.currencyconversionservice.model.response.CurrencyConversionResponse;
+import com.qirara.currencyconversionservice.proxy.CurrencyExchangeProxy;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -14,6 +15,12 @@ import java.util.Map;
 @RestController
 public class CurrencyConversionController {
 
+    private final CurrencyExchangeProxy currencyExchangeProxy;
+
+    public CurrencyConversionController(CurrencyExchangeProxy currencyExchangeProxy) {
+        this.currencyExchangeProxy = currencyExchangeProxy;
+    }
+
     @GetMapping(path = "/currency-conversion/from/{from}/to/{to}/quantity/{quantity}")
     public ResponseEntity<CurrencyConversionResponse> retrieveConversion(@PathVariable String from, @PathVariable String to, @PathVariable BigDecimal quantity) {
 
@@ -24,7 +31,7 @@ public class CurrencyConversionController {
         String URI = "http://localhost:8000/currency-exchange/from/{from}/to/{to}";
         ResponseEntity<CurrencyConversionResponse> currencyConversionResponse = new RestTemplate().getForEntity(URI, CurrencyConversionResponse.class, uriVariable);
         CurrencyConversionResponse currencyConversion = currencyConversionResponse.getBody();
-       
+
         return ResponseEntity.ok(new CurrencyConversionResponse(
                 currencyConversion.getId(),
                 from,
@@ -32,7 +39,24 @@ public class CurrencyConversionController {
                 quantity,
                 currencyConversion.getCurrencyMultiple(),
                 quantity.multiply(currencyConversion.getCurrencyMultiple()),
-                currencyConversion.getEnvironment()
+                currencyConversion.getEnvironment().concat("-rest templete")
+        ));
+    }
+
+    @GetMapping(path = "/currency-conversion-feign/from/{from}/to/{to}/quantity/{quantity}")
+    public ResponseEntity<CurrencyConversionResponse> retrieveConversionFeign(@PathVariable String from, @PathVariable String to, @PathVariable BigDecimal quantity) {
+
+        ResponseEntity<CurrencyConversionResponse> currencyConversionResponse = currencyExchangeProxy.retrieveCurrencyExchange(from, to);
+        CurrencyConversionResponse currencyConversion = currencyConversionResponse.getBody();
+
+        return ResponseEntity.ok(new CurrencyConversionResponse(
+                currencyConversion.getId(),
+                from,
+                to,
+                quantity,
+                currencyConversion.getCurrencyMultiple(),
+                quantity.multiply(currencyConversion.getCurrencyMultiple()),
+                currencyConversion.getEnvironment().concat("-feign")
         ));
     }
 
---
+++ currency-conversion-service/src/main/java/com/qirara/currencyconversionservice/proxy/CurrencyExchangeProxy.java
@@ -0,0 +1,17 @@
+package com.qirara.currencyconversionservice.proxy;
+
+import com.qirara.currencyconversionservice.model.response.CurrencyConversionResponse;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@FeignClient(name = "currency-exchange", url = "http://localhost:8000")
+public interface CurrencyExchangeProxy {
+
+    @RequestMapping(method = RequestMethod.GET, path = "/currency-exchange/from/{from}/to/{to}", consumes = "application/json")
+    ResponseEntity<CurrencyConversionResponse> retrieveCurrencyExchange(@PathVariable String from, @PathVariable String to);
+
+}
