b47660644f8d3cda77dfa37e3fa7816b5fc4f1b1
Ariq Khoiri <qq.khoiri@gmail.com>
2023-03-25 09:24:58 +0700

    feat:load balancing using spring cloud loadbalancing

--- currency-conversion-service/src/main/java/com/qirara/currencyconversionservice/proxy/CurrencyExchangeProxy.java
+++ currency-conversion-service/src/main/java/com/qirara/currencyconversionservice/proxy/CurrencyExchangeProxy.java
@@ -8,7 +8,8 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
-@FeignClient(name = "currency-exchange")
+// @FeignClient(name = "currency-exchange", host = "http://localhost:8000") // not load balancing
+@FeignClient(name = "currency-exchange") // automatic load balancing using spring cloud load balancing
 public interface CurrencyExchangeProxy {
 
     @RequestMapping(method = RequestMethod.GET, path = "/currency-exchange/from/{from}/to/{to}", consumes = "application/json")
