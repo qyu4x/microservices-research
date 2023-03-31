aa93c8d6cddc9738d53ba7d0662d1012fec1928d
Ariq Khoiri <qq.khoiri@gmail.com>
2023-03-31 13:10:28 +0700

    feat:global logging

---
+++ api-gateway/src/main/java/com/qirara/apigateway/config/LoggingConfiguration.java
@@ -0,0 +1,27 @@
+package com.qirara.apigateway.config;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.cloud.gateway.filter.GatewayFilterChain;
+import org.springframework.cloud.gateway.filter.GlobalFilter;
+import org.springframework.stereotype.Component;
+import org.springframework.web.server.ServerWebExchange;
+import reactor.core.publisher.Mono;
+
+import java.time.LocalDateTime;
+
+@Component
+public class LoggingConfiguration implements GlobalFilter {
+    // global filter
+    // spring cloud gateway based on spring webflux so mostly use reactive concept
+
+
+    private final Logger logger = LoggerFactory.getLogger(this.getClass());
+
+    @Override
+    public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
+        logger.info("Path of request received -> {} [{}]", exchange.getRequest().getPath(), LocalDateTime.now());
+        return chain.filter(exchange);
+    }
+
+}
